f0c7fbe1b3b301788e8c2911c567cafdb99fc552
jesse kaminsky <jck69@pitt.edu>
2017-02-11 01:41:17 -0500

    hah dan :D

--- TreeTester.java
+++ TreeTester.java
@@ -1,24 +1,77 @@
 import java.io.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
 
-public class TreeTester
+public class Ghost implements Speechlet
 {
-	public static void main(String[] args)
-	{
-		Tree dict = new Tree();
-		try
-		{
-			BufferedReader br = new BufferedReader(new FileReader("usableWords.txt"));
-			String line = br.readLine();
-			while(line != null)
-			{
-				dict.add(line);
-				line = br.readLine();
+	Tree dict = new Tree();
+	String soFar;
+	@Override
+	public void onSessionStarted(final SessionStartedRequest request, final Session session)
+            throws SpeechletException
+            {
+        		log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+				try
+				{
+					BufferedReader br = new BufferedReader(new FileReader("usableWords.txt"));
+					String line = br.readLine();
+					soFar = line;
+					line = br.readLine();
+					while(line != null)
+					{
+						dict.add(line);
+						line = br.readLine();
+					}
+
+				}
+				catch(IOException e)
+				{
+					System.out.println(e.getMessage());
+				}
 			}
-		}
-		catch(IOException e)
-		{
-			System.out.println(e.getMessage());
-		}
+			@Override
+    		public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
+            throws SpeechletException
+            {
+        		log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        		return getWelcomeResponse();
+    		}
+    		@Override
+    		public SpeechletResponse onIntent(final IntentRequest request, final Session session)
+            throws SpeechletException
+            {
+        		log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+		        Intent intent = request.getIntent();
+		        String intentName = (intent != null) ? intent.getName() : null;
+
+		        if ("HelloWorldIntent".equals(intentName))
+		        {
+		            return getHelloResponse();
+		        }
+		        else if ("AMAZON.HelpIntent".equals(intentName))
+		        {
+		            return getHelpResponse();
+		        }
+		        else
+		        {
+		            throw new SpeechletException("Invalid Intent");
+		        }
+    		}
 	}
 
 	public static class Tree
